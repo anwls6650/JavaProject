577573c484e9e810bab2ed6e31b7447676e3cefa
kangmujin <anwls6650@gmail.com>
2023-09-14 20:33:14 +0900

    2023-09-14 20:33 commit

---
+++ JavaProject/src/java/seo03/Main.java
@@ -0,0 +1,16 @@
+package java.seo03;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Slime slime1 = new Slime();
+        Slime slime2 = new Slime();
+
+        slime1.attack(slime2);
+
+	}
+
+}
